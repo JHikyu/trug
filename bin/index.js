8b2596f397d9a5e1d8a09a62076e66e9088f3c13
Justin <therosti@gmail.com>
2022-02-10 02:01:12 +0100

    Public, WebSocket and more!

--- bin/index.js
+++ bin/index.js
@@ -1,167 +1,66 @@
 #!/usr/bin/env node
 
-// openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+//* Node Requirements
+    const http = require('http');
+    const https = require('https');
+    const fs = require('fs');
+    const path = require("path");
+//****************************************************************/
 
-// Requirements
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const fs = require('fs');
-const path = require("path");
+//* External Requirements
+    const argv = require('minimist')(process.argv.slice(2));
+    const color = require('colors');
+//****************************************************************/
 
-const argv = require('minimist')(process.argv.slice(2));
-const colors = require('colors');
+//* Config
+    const PORT = isNaN(argv.port) ? 80 : argv.port || argv.p || 80;
+    const HTTPS_PORT = isNaN(argv.sslport) ? 443 : argv.sslport || 443;
+    const PROJECT_PATH = path.resolve(argv.path ? argv.path : '.');
+    const ssl = argv.ssl == true ? {
+        key: fs.readFileSync(path.join(PROJECT_PATH, 'ssl', 'key.pem')),
+        cert: fs.readFileSync(path.join(PROJECT_PATH, 'ssl', 'cert.pem'))
+    } : {};
+//****************************************************************/
 
-const ejs = require('ejs');
-const pug = require('pug');
 
-const PORT = isNaN(argv.port) ? 80 : argv.port || argv.p || 80;
-const HTTPS_PORT = isNaN(argv.sslport) ? 443 : argv.sslport || 443;
-const PROJECT_PATH = argv.path || '.';
-const ssl = argv.ssl == true ? {
-    key: fs.readFileSync(path.join(PROJECT_PATH, 'ssl', 'key.pem')),
-    cert: fs.readFileSync(path.join(PROJECT_PATH, 'ssl', 'cert.pem'))
-} : {};
-log(`Booting Trug 🧺 in ${PROJECT_PATH}`);
+//* Lib
+    const { log } = require('../lib/utils');
+    const fullServer = require('../lib/fullServer');
+    const webSocket = require('../lib/webSocket');
+//****************************************************************/
+
+
+//* Startup
+log(`Booting Trug in ${PROJECT_PATH}`);
 
 // Check if project is setup correctly
 log('Checking project folders...');
-log(`views folder: ${PROJECT_PATH}/views ${fs.existsSync(`${PROJECT_PATH}/views`) ? '✓' : '✗'}`);
-log(`public folder: ${PROJECT_PATH}/public ${fs.existsSync(`${PROJECT_PATH}/public`) ? '✓' : '✗'}`);
-log(`src folder: ${PROJECT_PATH}/src ${fs.existsSync(`${PROJECT_PATH}/src`) ? '✓' : '✗'}`);
-
-if(!fs.existsSync(`${PROJECT_PATH}/views`) || !fs.existsSync(`${PROJECT_PATH}/public`) || !fs.existsSync(`${PROJECT_PATH}/src`)) {
+log(`views folder: ${PROJECT_PATH}/views ${fs.existsSync(PROJECT_PATH + '/views') ? 'found'.green : 'not found'.red}`);
+log(`public folder: ${PROJECT_PATH}/public ${fs.existsSync(PROJECT_PATH + '/public') ? 'found'.green : 'not found'.red}`);
+log(`src folder: ${PROJECT_PATH}/src ${fs.existsSync(PROJECT_PATH + '/src') ? 'found'.green : 'not found'.red}`);
+if(!fs.existsSync(`${PROJECT_PATH}/views`)) {
     log('Project not setup correctly. Please run '.red + '`trug-init`'.yellow + ' to setup your project.'.red);
     process.exit(1);
 }
 
-// Run http server
 
-async function fullServer(req, res) {
-    const { pathname } = url.parse(req.url, true);
-    const fullPath = path.join(PROJECT_PATH, 'views', pathname, pathname === '/' ? 'index' : '');
-
-    //? Favicon redirect to public/favicon.ico
-    if(pathname === '/favicon.ico' && fs.existsSync(path.join(PROJECT_PATH, 'public', 'favicon.ico'))) {
-        res.setHeader('Content-Type', 'image/x-icon');
-        fs.createReadStream(path.join(PROJECT_PATH, 'public', 'favicon.ico')).pipe(res);
-        return;
-    }
-
-    //? Check if file is found at path
-    log(`request on ${pathname} checking ${fullPath}`);
-    const extension = fileExtension(fullPath) ?? fileExtension(path.join(fullPath, 'index'));
-    const index = !fileExtension(fullPath);
-
-    if(!extension) {
-        //? Check if it's a file in public
-        if(fs.existsSync(path.join(PROJECT_PATH, 'public', pathname))) {
-            log(`serving ${pathname} from public folder`);
-            res.setHeader('Content-Type', 'text/html');
-            fs.createReadStream(path.join(PROJECT_PATH, 'public', pathname)).pipe(res);
-            return;
-        }
-
-        log(`${pathname} not found`.underline);
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end(`Cannot ${req.method} ${req.url}`);
-        return;
-    }
-    const fullFilePath = `${fullPath}${index ? '\\index' : ''}.${extension}`;
-    log(`${fullFilePath} found`.underline);
-    
-
-    //? Run script of route
-    const fullFilePathJs = `${fullPath}${index ? '\\index' : ''}`;
-    let script;
-    if(fs.existsSync(fullFilePathJs + '.js')) {
-        // nocache(path.resolve(`${fullFilePathJs}.js`));
-
-        try {
-            script = await require(path.resolve(`${fullFilePathJs}.js`))();
-        } catch (error) {
-            log(`Error running script ${fullFilePathJs}.js`.red);
-            log(error.stack.red);
-        }
-
-        log(`${fullFilePathJs} found`.underline);
-    }
-
-    //? Render file
-    fs.readFile(fullFilePath, (err, data) => {
-        if(err) {
-            log(`${pathname} not found`.underline);
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end(`Cannot ${req.method} ${req.url}`);
-            return;
-        }
-
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        if(extension === 'pug') {
-            fn = pug.compile(data, {
-                filename: fullFilePath,
-                pretty: true
-            });
-            res.end(fn(script?.data));
-        }
-        else if(extension === 'ejs') {
-            fn = ejs.compile(fullFilePath.toString());
-            res.end(fn(script?.data));
-        }
-        else if(extension === 'html') res.end(data);
-        else res.end('');
-
-        return;
-    });
-}
 
 if(ssl.key) {
     https.createServer(ssl, async (req, res) => {
-        fullServer(req, res);
+        fullServer(req, res, server);
     }).listen(HTTPS_PORT, () => {
-        console.log(`Trug 🧺 is running on port ${HTTPS_PORT} 👍 via ssl!`.green.bold);
+        console.log(`Trug is running on port ${HTTPS_PORT} via ssl!`.green.bold);
     });
 }
-http.createServer(async (req, res) => {
-    fullServer(req, res);
+const server = http.createServer(async (req, res) => {
+    fullServer(req, res, server);
 }).listen(PORT, () => {
-    console.log(`Trug 🧺 is running on port ${PORT} 👍`.green.bold);
+    console.log(`Trug is running on port ${PORT}`.green.bold);
 });
 
+webSocket.init(server);
 
-function fileExtension(path) {
-    if(fs.existsSync(path + '.pug'))
-        return 'pug';
-    if(fs.existsSync(path + '.html'))
-        return 'html';
-    if(fs.existsSync(path + '.ejs'))
-        return 'ejs';
-    return undefined;
+// Look for socket.js in root
+if(fs.existsSync(`${PROJECT_PATH}/socket.js`)) {
+    const socket = require(`${PROJECT_PATH}/socket.js`)(webSocket);
 }
-function log(msg) {
-    console.log(`[LOG]`.bgGray, msg.grey);
-}
-
-
-
-
-(async function fromDir(startPath, filter){
-    if (!fs.existsSync(startPath)){
-        return;
-    }
-    var files = fs.readdirSync(startPath);
-    for(var i = 0 ; i < files.length ; i++) {
-        var filename = path.join(startPath,files[i]);
-        var stat = await fs.lstatSync(filename);
-        if (stat.isDirectory()) {
-            fromDir(filename, filter); //recurse
-        }
-        else if (filename.endsWith(filter)) {
-            log(`watching ${filename}`);
-            fs.watchFile(path.resolve(filename), () => {
-                log(`${filename} changed`.yellow);
-                delete require.cache[path.resolve(filename)];
-            });
-        }
-    }
-})(path.join(PROJECT_PATH), '.js');
--- bin/init.js
+++ bin/init.js
@@ -29,7 +29,7 @@ const PROJECT_PATH = argv.path || '.';
     }
     ]);
   
-    if(response.basicFolders) {
+    if(response.basicFolders || response.starterProject) {
         // Create basic folders
         fs.mkdirSync(`${PROJECT_PATH}/views`);
         fs.mkdirSync(`${PROJECT_PATH}/public`);
@@ -39,26 +39,63 @@ const PROJECT_PATH = argv.path || '.';
         // Create starter project
         // create file
         fs.mkdirSync(`${PROJECT_PATH}/views/api`);
-        fs.writeFileSync(`${PROJECT_PATH}/views/api/index.html`, `localhost/api`);
-        fs.writeFileSync(`${PROJECT_PATH}/views/api/endpoint.html`, `localhost/api/endpoint`);
-        fs.writeFileSync(`${PROJECT_PATH}/views/index.html`, ``);
-        fs.writeFileSync(`${PROJECT_PATH}/views/index.js`,
-        `
-        module.exports = async () => {
-            //* Run once on page load [index.html]
-            
-            // Data to render in a template like ejs or pug
-            // return {
-            //     data: {
-            //         title: '$$TEST TITLE VARIABLE$$',
-            //         logs: logLength
-            //     }
-            // };
-        };
-        `
-        );
-    }
+        fs.writeFileSync(`${PROJECT_PATH}/views/api/index.pug`,
+`doctype html
+html(lang="en")
+    head
+        meta(charset="UTF-8")
+        meta(http-equiv="X-UA-Compatible", content="IE=edge")
+        meta(name="viewport", content="width=device-width, initial-scale=1.0")
+        title Document
+        link(rel="stylesheet", href="/style.css")
+    body 
+        p localhost/api
+        p= title
+        p= someVariables
+`);
+        fs.writeFileSync(`${PROJECT_PATH}/views/api/index.js`,
+`module.exports = function() {
+    //* Run once on page load [api/index.pug]
+    console.log('hello world from api/index.js!');
 
+    return {
+        data: {
+            title: 'Trug 🧺',
+            someVariable: 123
+        }
+    };
+}`);
+        fs.writeFileSync(`${PROJECT_PATH}/views/api/endpoint.js`,
+`module.exports = function(trug) {
+    return {
+        method: 'GET',
+        data: trug
+    }
+}`);
+        fs.writeFileSync(`${PROJECT_PATH}/views/index.html`,
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    Landing page runnig on trug 🧺
+    reachable > localhost/
+</body>
+</html>`);
+        fs.writeFileSync(`${PROJECT_PATH}/public/style.css`,
+`body {
+    font-family: Arial, Helvetica, sans-serif;
+}`);
+        fs.writeFileSync(`${PROJECT_PATH}/views/index.js`,
+`module.exports = async (trug) => {
+    //* Run once on page load [index.html]
+};`);
+    }
 
     console.log(`Enjoy your new project!`.green);
     console.log('Run ' + '`trug`'.yellow + ' to start your server');
